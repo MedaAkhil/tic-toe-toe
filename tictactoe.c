aa9e99af4f9a15c3f2ada25a524714df1a16c506
MedaAkhil <94841034+MedaAkhil@users.noreply.github.com>
2022-12-06 16:46:15 +0530

    Add files via upload
    Final game commit

---
+++ tictactoe.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <conio.h>
+#include<string.h>
+
+char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+int checkwin();
+void board();
+
+int main()
+{
+    int  i, choice,player = 1;
+    char playername[20];
+    char p1[20],p2[20];
+    char mark;
+    printf("Enter the first player name:");
+        scanf("%s",p1);
+        printf("Enter the second player name:");
+        scanf("%s",p2);
+    do
+    {
+        board(p1,p2);
+        (player % 2) ? strcpy(playername,p1) : strcpy(playername,p2);
+        player=(player % 2) ? 1: 2;
+        printf("Player %s, enter a number:  ", playername);
+        scanf("%d", &choice);
+
+        mark = (player == 1) ? 'X' : 'O';
+
+        if (choice == 1 && square[1] == '1')
+            square[1] = mark;
+            
+        else if (choice == 2 && square[2] == '2')
+            square[2] = mark;
+            
+        else if (choice == 3 && square[3] == '3')
+            square[3] = mark;
+            
+        else if (choice == 4 && square[4] == '4')
+            square[4] = mark;
+            
+        else if (choice == 5 && square[5] == '5')
+            square[5] = mark;
+            
+        else if (choice == 6 && square[6] == '6')
+            square[6] = mark;
+            
+        else if (choice == 7 && square[7] == '7')
+            square[7] = mark;
+            
+        else if (choice == 8 && square[8] == '8')
+            square[8] = mark;
+            
+        else if (choice == 9 && square[9] == '9')
+            square[9] = mark;
+            
+        else
+        {
+            printf("Invalid move ");
+
+            player--;
+            getch();
+        }
+        i = checkwin();
+
+        player++;
+    }while (i ==  - 1);
+    
+    board(p1,p2);
+    
+    if (i == 1){
+        --player;
+        (player % 2) ? strcpy(playername,p1) : strcpy(playername,p2);
+        printf("==>\a %s won the match ",playername );
+    }
+    else
+        printf("==>\aGame draw");
+
+    getch();
+
+    return 0;
+}
+
+
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+        return 1;
+        
+    else if (square[4] == square[5] && square[5] == square[6])
+        return 1;
+        
+    else if (square[7] == square[8] && square[8] == square[9])
+        return 1;
+        
+    else if (square[1] == square[4] && square[4] == square[7])
+        return 1;
+        
+    else if (square[2] == square[5] && square[5] == square[8])
+        return 1;
+        
+    else if (square[3] == square[6] && square[6] == square[9])
+        return 1;
+        
+    else if (square[1] == square[5] && square[5] == square[9])
+        return 1;
+        
+    else if (square[3] == square[5] && square[5] == square[7])
+        return 1;
+        
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
+        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
+        != '7' && square[8] != '8' && square[9] != '9')
+
+        return 0;
+    else
+        return  - 1;
+}
+
+
+
+
+void board(char p1[20],char p2[20])
+{
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player %s (X)  -  Player %s (O)\n\n\n",p1,p2);
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
+
+    printf("     |     |     \n\n");
+}
